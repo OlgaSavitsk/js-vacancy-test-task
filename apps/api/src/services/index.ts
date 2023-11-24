7f0d431d0b4ee4c4eae14bd2902d63cd675f9855
Olga <olbonya.89@mail.ru>
2023-11-24 15:52:36 +0300

    fix: delete redis url env

--- apps/api/src/services/index.ts
+++ apps/api/src/services/index.ts
@@ -4,7 +4,6 @@ import cloudStorageService from './cloud-storage/cloud-storage.service';
 import emailService from './email/email.service';
 import googleService from './google/google.service';
 import rateLimitService from './rate-limit/rate-limit.service';
-// import socketService from './socket/socket.service';
 
 export {
   analyticsService,
@@ -13,5 +12,4 @@ export {
   emailService,
   googleService,
   rateLimitService,
-  // socketService,
 };
